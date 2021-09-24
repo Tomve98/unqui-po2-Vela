d059279577d7b25f45b0e2f731c554928cc3f3f7
Tomas Vela <tomasfranciscovela@gmail.com>
2021-09-23 22:17:36 -0300

    nada

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -2,7 +2,7 @@ package ar.edu.unq.po2.tp3;
 
 public class Counter {
 }
-
+ 
 
 //Los tipos de datos primitivos son aquellos que no necesitan ser declarados y se almacena como un valor literal
 //int es un tipo de dato primitivo de Java que hace referencia a un numero entero, en cambio Integer es una clase o un metodo el cual puede o no contener un int
