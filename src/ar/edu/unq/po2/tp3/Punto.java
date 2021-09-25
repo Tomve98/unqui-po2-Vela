7c74de7f374f37db3e79ccf5146ca7556923fd24
Tomas Vela <tomasfranciscovela@gmail.com>
2021-09-24 22:49:48 -0300

    nada

---
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -0,0 +1,19 @@
+///8
+
+package ar.edu.unq.po2.tp3;
+
+public class Punto {
+	int x;
+	int y;
+	
+
+	public Punto(int x, int y) {//1
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Punto() {//2
+		
+	}
+	
+}
